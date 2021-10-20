5ecefa5141e08370a1cb61337e3ca4f81fb40ac3
Alex Paredes <alexparedesr95@gmail.com>
2021-10-19 22:10:30 -0500

    FEAT: Se crean rutas para las páginas Flex y Typography.

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'typography', component: () => import('pages/Typography.vue') },
+      { path: 'flex', component: () => import('pages/Flex.vue') },
     ]
   },
 
