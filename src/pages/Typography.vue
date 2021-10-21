3c601c20caef398ffa634152d68a87d76d2d0c63
Alex Paredes <alexparedesr95@gmail.com>
2021-10-20 20:53:16 -0500

    FEAT: Se explora la tipografía de Quasar.

--- src/pages/Typography.vue
+++ src/pages/Typography.vue
@@ -1,7 +1,31 @@
 <template>
-  <q-page class="flex">
+  <q-page class="flex column q-pa-md">
     
-    <h1>Typography</h1>
+    <span class="text-h1">Headline 1</span>
+    <span class="text-h2">Headline 2</span>
+    <span class="text-h3">Headline 3</span>
+    <span class="text-h4">Headline 4</span>
+    <span class="text-h5">Headline 5</span>
+    <span class="text-h6">Headline 6</span>
+
+    <q-separator />
+
+    <span class="text-subtitle1">Subtitle 1</span>
+    <span class="text-subtitle2">Subtitle 2</span>
+
+    <q-separator />
+
+    <p class="text-body1">Elit excepteur commodo eiusmod Lorem tempor nisi cillum do.</p>
+    <p class="text-body2">Culpa duis pariatur sint minim commodo.</p>
+
+    <q-separator />
+
+<span class="text-weight-thin">Qui irure ipsum nostrud aute nulla dolor ipsum nostrud excepteur.</span>
+<span class="text-weight-light">Irure mollit exercitation do irure velit id sit nisi consequat dolor fugiat sunt.</span>
+<span class="text-weight-regular">Officia nisi Lorem nulla magna consequat.</span>
+<span class="text-weight-medium">Adipisicing esse nostrud sunt consectetur sit mollit veniam.</span>
+<span class="text-weight-bold">Reprehenderit fugiat reprehenderit id aliqua duis sint exercitation in est anim deserunt adipisicing occaecat.</span>
+<span class="text-weight-bolder">Est amet deserunt nisi officia anim incididunt enim sint ea incididunt.</span>
 
   </q-page>
 </template>
