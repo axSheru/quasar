eff484e07e51004234ef8f3f0180044edf573dd9
Alex Paredes <alexparedesr95@gmail.com>
2021-10-20 21:03:45 -0500

    FEAT: Se separa la lista de links para mantener más limpio el layout.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
           Quasar App
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <!-- <div>Quasar v{{ $q.version }}</div> -->
       </q-toolbar>
     </q-header>
 
@@ -32,7 +32,7 @@
         </q-item-label>
 
         <EssentialLink
-          v-for="link in essentialLinks"
+          v-for="link in linksList"
           :key="link.title"
           v-bind="link"
         />
@@ -47,27 +47,7 @@
 
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Typography',
-    caption: 'Tipos de letra en Quasar',
-    icon: 'las la-align-left',
-    link: 'typography'
-  },
-  {
-    title: 'Flex',
-    caption: 'Estilos con Flex',
-    icon: 'las la-layer-group',
-    link: 'flex'
-  },
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'las la-graduation-cap',
-    link: 'https://quasar.dev'
-  },
-];
+import { linksList } from '../router/link-list'
 
 import { defineComponent, ref } from 'vue'
 
@@ -82,7 +62,7 @@ export default defineComponent({
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
+      linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
