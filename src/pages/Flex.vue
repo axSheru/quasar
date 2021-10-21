8b7cdaa649f30274853383763411d596ae1921b6
Alex Paredes <alexparedesr95@gmail.com>
2021-10-20 22:10:19 -0500

    FEAT: Se juega con el sistema de filas y columnas.

--- src/pages/Flex.vue
+++ src/pages/Flex.vue
@@ -1,7 +1,32 @@
 <template>
-  <q-page class="flex">
+  <q-page>
     
-    <h1>Flex</h1>
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col bg-secondary"></div>
+      <div class="col bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col col-5 bg-secondary"></div>
+      <div class="col col-6 bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-center">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-md justify-end">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-md justify-between">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
 
   </q-page>
 </template>
@@ -13,3 +38,11 @@ export default defineComponent({
   name: 'Flex'
 })
 </script>
+
+<style scoped>
+
+.col, .col-4, .col-6, .col-12 {
+  height: 50px;
+}
+
+</style>
