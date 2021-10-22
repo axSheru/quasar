06c714eea3e1c52991f8e18a1cf760e8d544703e
Alex Paredes <alexparedesr95@gmail.com>
2021-10-21 21:31:12 -0500

    FEAT: Se crea página forms y su navegación.

---
+++ src/pages/Forms.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="q-ma-md">
+    
+    <span class="text-h3">Forms</span>
+    <q-separator spaced />
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Forms'
+})
+</script>
